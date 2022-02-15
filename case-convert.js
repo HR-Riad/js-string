ac96a84469a04b167bf467d3f462ff4cfb33b94b
Md. Hasanur Rahman Riad <57297996+HR-Riad@users.noreply.github.com>
2022-02-16 01:04:40 +0600

    String comparison using toLowerCase toUpperCase

---
+++ case-convert.js
@@ -0,0 +1,11 @@
+const anthem = 'Amar Sonar Bangla Ami Tomai Valobashi';
+const search = 'valo';
+
+
+const userInput = 'blACkPiNK';
+const savedUserName = 'blAckPink';
+// console.log(userInput.toLowerCase());
+// console.log(savedUserName.toLowerCase());
+if (userInput. toUpperCase() == savedUserName. toUpperCase()) {
+    console.log('user exists');
+}
